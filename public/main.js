7c08974cafd81e0ba5b2f6a61e22bd2c3d0d2c6f
michaelandom03 <michaelandom03@gmail.com>
2021-12-23 10:40:56 +0300

    all done

--- public/main.js
+++ public/main.js
@@ -1,8 +1,7 @@
 const { app, BrowserWindow } = require('electron')
 
-// const path = require('path')
-// const isDev = require('electron-is-dev')
-
+const path=require('path');
+const isDev=require('electron-is-dev')
 require('@electron/remote/main').initialize()
 
 function createWindow() {
@@ -11,29 +10,25 @@ function createWindow() {
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true,
+      nodeIntegration:true,
       enableRemoteModule: true
     }
   })
 
   win.loadURL(
-    'http://localhost:3000'
+    isDev?'http://localhost:3000':
+    `file://${path.join(__dirname,'../build/index.html')}`
   )
 }
 
 app.on('ready', createWindow)
 
-// Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
 app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) createWindow()
 })
--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import {useForm} from "react-hook-form"; 
+import { Button,Form } from 'react-bootstrap';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     const [answer,setAnswer] =useState();
+const {register,handleSubmit,formState: { errors } } =useForm({defaultValues:{}});
+const onSubmit=({lenth1,quantity1,lenth2,quantity2})=>{
+var total=(quantity1*lenth1)+(quantity2*lenth2);
+var answer=0;
+var rem=total%12;
+if(rem===0){
+answer=total/12;
+}else{
+     answer=(Math.floor(total/12))+1;
+}
+setAnswer(`${answer} rebars are needed and ${rem===0?0:12-rem} meter will end up as wastage`);
+}
+return (
+     <div className="w-75 m-auto  p-3 shadow-lg">
+          <h3 className="text-primary ">Bar calculator</h3>
+     <Form onSubmit={handleSubmit(onSubmit)}>
+<Form.Group className="mb-3" controlId="formBasicNumber">
+    <Form.Label>length 1</Form.Label>
+    <Form.Control type="number" placeholder="Enter length 1"{...register("lenth1",{ required: true })} onChange={()=>setAnswer()} />
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicNumber">
+    <Form.Label>quantity 1</Form.Label>
+    <Form.Control type="number" placeholder="Enter quantity 1"{...register("quantity1",{ required: true })} onChange={()=>setAnswer()} />
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicNumber">
+    <Form.Label>length 2</Form.Label>
+    <Form.Control type="number" placeholder="Enter length 2"{...register("lenth2",{ required: true })} onChange={()=>setAnswer()} />
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicNumber">
+    <Form.Label>quantity 2</Form.Label>
+    <Form.Control type="number" placeholder="Enter quantity 2"{...register("quantity2",{ required: true })} onChange={()=>setAnswer()} />
+  </Form.Group>
+   <br/>
+     <Button type="submit" variant="primary w-100">calculate</Button>
+     <br/>
+     <br/>
+     <br/>
+     {(errors.lenth1||errors.lenth2||errors.quantity1||errors.quantity2) && <span className="text-danger shadow-lg p-2">This field is required</span>}
+     {answer && <span className="text-success  p-2">{answer}</span>}
+          </Form>
+          </div>
   );
 }
 
